8116d52ec24926366bc71e7f726bd96888c1a7c5
ajay <ajay@AJAY-PC>
2018-02-22 10:53:22 +0530

    Modified the footer component , added the next , previous button functionalities

---
+++ VARC/VARC/src/app/services/dataService.js
@@ -0,0 +1,59 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var DataService = /** @class */ (function () {
+    function DataService() {
+    }
+    DataService.prototype.testMethod = function () {
+        //return 'test success';
+        return [{
+                name: '11Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            },
+            {
+                name: '22Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            },
+            {
+                name: '33Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            },
+            {
+                name: '44Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            },
+            {
+                name: '55Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            }, {
+                name: '66Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            }, {
+                name: '77Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            }, {
+                name: '88Independent House',
+                location: 'jksadghfjk',
+                client: 'wjed'
+            }];
+    };
+    DataService = __decorate([
+        core_1.Injectable()
+    ], DataService);
+    return DataService;
+}());
+exports.DataService = DataService;
+//# sourceMappingURL=dataService.js.map
---
+++ VARC/VARC/src/app/services/dataService.ts
@@ -0,0 +1,48 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class DataService {
+
+    testMethod() {
+        //return 'test success';
+
+        return [{
+            name: '11Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        },
+        {
+            name: '22Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        },
+        {
+            name: '33Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        },
+        {
+            name: '44Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        },
+        {
+            name: '55Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        }, {
+            name: '66Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        }, {
+            name: '77Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        }, {
+            name: '88Independent House',
+            location: 'jksadghfjk',
+            client: 'wjed'
+        }]
+    }
+}
